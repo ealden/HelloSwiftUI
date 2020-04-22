c1ef9d8c8a1c85b3f3525bfc0d8fa524261276e5
Ealden Esto E. Escañan <ealden@escanan.com>
2020-04-23 05:23:38 +0800

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ealden Escañan on 4/23/20.
+//  Copyright © 2020 Ealden Escañan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

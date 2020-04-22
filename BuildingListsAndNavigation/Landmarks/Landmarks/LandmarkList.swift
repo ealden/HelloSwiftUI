7fb86f1763bcdc689a221e1f1f558674d8c11bf4
Ealden Esto E. Escañan <ealden@escanan.com>
2020-04-23 06:45:55 +0800

    Section 8: Generating Previews Dynamically

--- BuildingListsAndNavigation/Landmarks/Landmarks/LandmarkList.swift
+++ BuildingListsAndNavigation/Landmarks/Landmarks/LandmarkList.swift
@@ -23,6 +23,11 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone 11 Pro Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+        
     }
 }
